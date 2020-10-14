a826005970304db423b31c8706da636a30dfe826
maksim <maximthebest@outlook.com>
2020-10-15 00:29:11 +0300

    Added ChessBoard constructor, place_chess_piece and chess_piece_at methods (and tests)

--- lib/chessboard.rb
+++ lib/chessboard.rb
@@ -5,12 +5,38 @@ class Chessboard
   def self.decode_pos(str)
     raise 'expected string' unless str.is_a?(String)
     raise "invalid position: #{str}" unless str =~ /^[a-h][1-8]$/
+
     [str[0].ord - 'a'.ord, str[1].to_i - 1]
   end
 
   def self.encode_pos(array)
     raise 'expected array' unless array.is_a?(Array)
     raise "invalid position #{array}" unless array.size == 2 && array[0].between?(0, 7) && array[1].between?(0, 7)
+
     [(array[0] + 'a'.ord).chr, (array[1] + 1).to_s].join
+  end
+
+  # Creates an empty chess board
+  def initialize()
+    @board = Array.new(8) { Array.new(8, nil) }
+  end
+
+  # Returns chess piece or nil if cell is empty
+  def chess_piece_at(position)
+    position = Chessboard.decode_pos(position)
+    @board[position[0]][position[1]]
+  end
+
+  # Places chess piece at specified position
+  def place_chess_piece(chess_piece, position)
+    unless chess_piece.class.ancestors.include?(ChessPiece)
+      raise 'only ChessPiece objects or derived objects can be placed on ChessBoard'
+    end
+
+    position = Chessboard.decode_pos(position)
+
+    raise 'cell is not empty' unless @board[position[0]][position[1]].nil?
+
+    @board[position[0]][position[1]] = chess_piece
   end
 end
--- spec/chessboard_spec.rb
+++ spec/chessboard_spec.rb
@@ -1,5 +1,6 @@
 #spec/chessboard_spec.rb
 require './lib/chessboard'
+require './lib/chesspiece'
 
 describe Chessboard do
   describe '.decode_pos' do
@@ -27,6 +28,37 @@ describe Chessboard do
 
     it "returns 'h8' on [7, 7]" do
       expect(Chessboard.encode_pos([7, 7])).to eql('h8')
+    end
+  end
+
+  describe '#chess_piece_at' do
+    it 'returns nil if no chess piece was placed' do
+      chessboard = Chessboard.new
+      expect(chessboard.chess_piece_at('a2')).to eql(nil)
+    end
+
+    it 'returns chess piece that was placed here' do
+      chessboard = Chessboard.new
+      figure = ChessPiece.new('Some chess piece')
+      chessboard.place_chess_piece(figure, 'e7')
+      expect(chessboard.chess_piece_at('e7')).to eql(figure)
+    end
+  end
+
+  describe '#place_chess_piece' do
+    it 'places a chess piece on the board' do
+      chessboard = Chessboard.new
+      figure = ChessPiece.new('Some chess piece')
+      chessboard.place_chess_piece(figure, 'e7')
+      expect(chessboard.chess_piece_at('e7')).to eql(figure)
+    end
+
+    it 'fails when try to place object that is not of type ChessPiece' do
+      chessboard = Chessboard.new
+      def_not_a_chess_piece = 7
+      expect {
+        chessboard.place_chess_piece(def_not_a_chess_piece, 'e7')
+      }.to raise_error(RuntimeError)
     end
   end
 end
