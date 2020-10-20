ba701ff32b3a9f7c1a4084d654678a2e1b515fa4
maksim <maximthebest@outlook.com>
2020-10-20 19:51:29 +0300

    Changed two dimension array to one dimension array

--- lib/chessboard_grid.rb
+++ lib/chessboard_grid.rb
@@ -2,21 +2,35 @@
 # Container of 64 spaces for chess pieces
 # Allows indexing by ChessPosition object
 class ChessboardGrid
+  attr_reader :board
+
   # Creates an empty chess board
   def initialize
-    @board = Array.new(8) { Array.new(8, nil) }
+    @board = Array.new(64)
   end
 
   def [](position)
-    @board[position.i][position.j]
+    @board[position.i * 8 + position.j]
   end
 
   def []=(position, object)
-    @board[position.i][position.j] = object
+    @board[position.i * 8 + position.j] = object
   end
 
   def move(from, to)
     self[to] = self[from]
     self[from] = nil
+  end
+
+  def each_chess_piece
+    return to_enum(:each_chess_piece) unless block_given?
+    @board.each { |cell| yield(cell) unless cell.nil? }
+  end
+
+  def each_chess_piece_with_pos
+    return to_enum(:each_chess_piece_with_pos) unless block_given?
+    @board.each_with_index do | cell, index |
+      yield(cell, ChessPosition.from_i(index)) unless cell.nil?
+    end
   end
 end
