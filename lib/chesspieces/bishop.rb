1bcc11be5277fea2b06f549bcbc4b3034009a82a
maksim <maximthebest@outlook.com>
2020-10-23 12:27:10 +0300

    Bishop now uses DiagonalMoves module

--- lib/chesspieces/bishop.rb
+++ lib/chesspieces/bishop.rb
@@ -1,43 +1,11 @@
 require_relative 'chesspiece'
 require_relative '../chess_position'
+require_relative '../directional_moves'
 
 class Bishop < ChessPiece
-  @directions = [[1, 1], [1, -1], [-1, -1], [-1, 1]].map { |e| Point.from_a(e) }.freeze
-
-  class << self
-    attr_reader :directions
-  end
-
-  def directions
-    self.class.directions
-  end
+  include DiagonalMoves
 
   def available_moves(from, chessboard)
-    directions.reduce([]) do |moves_array, direction|
-      moves_array + available_moves_direction(from, chessboard, direction)
-    end
-  end
-
-  def available_moves_direction(from, chessboard, direction)
-    moves = []
-
-    current_cell = from + direction
-
-    loop do
-      break if current_cell.nil? # new cell is not on the chessboard
-
-      destination = chessboard.chess_piece_at(current_cell)
-
-      if destination.nil?
-        moves << current_cell
-      else
-        moves << current_cell if destination.color != color
-        break
-      end
-
-      current_cell = current_cell + direction
-    end
-
-    moves
+    available_moves_diagonal(from, chessboard.grid)
   end
 end
