ca6b093830385f54f77e7092fc296387fec913e1
maksim <maximthebest@outlook.com>
2020-10-30 23:08:29 +0200

    Added RandomMovePlayer class

--- lib/main.rb
+++ lib/main.rb
@@ -1,8 +1,9 @@
 require_relative 'chess_game'
 require_relative 'players/human_player'
+require_relative 'players/random_move_player'
 
 player1 = HumanPlayer.new('Red')
-player2 = HumanPlayer.new('Cyan')
+player2 = RandomMovePlayer.new('Cyan')
 
 game = ChessGame.new(player1, player2)
 
---
+++ lib/players/random_move_player.rb
@@ -0,0 +1,14 @@
+require_relative 'player'
+
+class RandomMovePlayer < Player
+  def move(chessboard, color)
+    all_moves = []
+    chessboard.allowed_moves(color).each_pair do |from, moves_from|
+      moves_from.each do |to|
+        all_moves << { from: from, to: to }
+      end
+    end
+
+    all_moves.sample
+  end
+end
