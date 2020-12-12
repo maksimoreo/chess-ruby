3360f48ebe17b9e1201b9fad284637effcafaf45
maksim <maximthebest@outlook.com>
2020-12-12 23:08:37 +0200

    Updated random_move_player.rb to use ChessMove

--- lib/players/random_move_player.rb
+++ lib/players/random_move_player.rb
@@ -2,13 +2,6 @@ require_relative 'player'
 
 class RandomMovePlayer < Player
   def move(chessboard)
-    all_moves = []
-    chessboard.allowed_moves(@color).each_pair do |from, moves_from|
-      moves_from.each do |to|
-        all_moves << { from: from, to: to }
-      end
-    end
-
-    all_moves.sample
+    chessboard.allowed_moves(@color).sample
   end
 end
